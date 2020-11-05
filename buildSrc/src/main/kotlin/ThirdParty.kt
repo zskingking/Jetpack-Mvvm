f6662039698c18e11e850e4a06114770513485b7
zs.bezier <495968956@qq.com>
2020-11-05 19:30:37 +0800

    修改okhttp版本

--- buildSrc/src/main/kotlin/ThirdParty.kt
+++ buildSrc/src/main/kotlin/ThirdParty.kt
@@ -6,7 +6,7 @@
 @Suppress("SpellCheckingInspection")
 object ThirdParty {
 
-    const val okhttp = "com.squareup.okhttp3:okhttp:3.14.9"
+    const val okhttp = "com.squareup.okhttp3:okhttp:4.9.0"
     const val rxjava = "io.reactivex.rxjava2:rxjava:2.2.14"
     const val rxAndroid = "io.reactivex.rxjava2:rxandroid:2.1.1"
     const val cookieJar = "com.github.franmontiel:PersistentCookieJar:v1.0.1"
